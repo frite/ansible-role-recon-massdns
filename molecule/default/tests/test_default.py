c9c2a35e0449e4568dbe3a0848b556e5a9857cca
Manolis Mavrofidis <emavrofid@ote.gr>
2019-11-25 14:12:16 +0200

    Init commit

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,16 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_massdns_file(host):
+    f = host.file('/usr/local/bin/massdns')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert oct(f.mode) == '0o755'
